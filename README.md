0298e1c94b318232961e3c33d6f9cb245389f710
ahonongobi <abyssiniea@gmail.com>
2024-06-21 17:07:08 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Machine-_Learning-assign-2_5_6_8
---
+++ my_git.sh
@@ -0,0 +1,6 @@
+echo "# Machine-_Learning-assign-2_5_6_8" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git remote add origin https://github.com/ahonongobi/Machine-_Learning-assign-2_5_6_8.git
+git push -u origin master
