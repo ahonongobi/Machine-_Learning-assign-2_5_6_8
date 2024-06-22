14e35e97246d56b7aeb2e4f86b3f892ea9c9e21a
ahonongobi <abyssiniea@gmail.com>
2024-06-22 10:21:48 +0200

    first commit added assign 6

--- my_git.sh
+++ my_git.sh
@@ -1,6 +1,6 @@
-echo "# Machine-_Learning-assign-2_5_6_8" >> README.md
-git init
+#echo "# Machine-_Learning-assign-2_5_6_8" >> README.md
+#git init
 git add .
-git commit -m "first commit"
-git remote add origin https://github.com/ahonongobi/Machine-_Learning-assign-2_5_6_8.git
+git commit -m "first commit added assign 6"
+#git remote add origin https://github.com/ahonongobi/Machine-_Learning-assign-2_5_6_8.git
 git push -u origin master
